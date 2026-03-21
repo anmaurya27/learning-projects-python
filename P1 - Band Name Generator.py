2498bf31021eccb87c1131438abcd24bfd72e927
Ankit Maurya <95664001+anmaurya27@users.noreply.github.com>
2026-03-21 15:35:51 +0530

    Add files via upload

---
+++ P1 - Band Name Generator.py
@@ -0,0 +1,15 @@
+#Project 1: Band Name Generator
+
+print("Welcome to the Band Name Generator!!")
+city_name = input("Type the name of the city you grew up in\n= ")
+animal_name = input("What is the name of your favourite animal?\n= ")
+print("Your Team Name is: " + city_name + " " + animal_name + "s" + 
+       "\nCongratulation! That's a very nice name.")
+
+
+# Python Concepts Learned / Practised:
+# - print() function
+# - input() function
+# - Variables
+# - Line break, \n
+# - String concatenation, +
